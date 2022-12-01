1a44a88a2e72520567302da21317dbd791b4e8a0
pooja <115702827+poojapradeep123@users.noreply.github.com>
2022-12-01 11:29:59 +0530

    Delete 0ver2.py

--- 0ver2.py
+++
@@ -1,9 +0,0 @@
-list=[]
-n=int(input("enter the number of elements"))
-for i in range(0,n):
-    list.append(int(input()))
-print("list is:",list)
-for i in range(0,n):
-    if list[i]>100:
-        list[i]='over'
-print("result list is:",list)
